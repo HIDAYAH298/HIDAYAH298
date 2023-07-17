b3ea9f8a7212f8447275e197abee2ff739e01058
HIDAYAH298 <139749285+HIDAYAH298@users.noreply.github.com>
2023-07-17 16:23:14 +0800

    PART C (b).c

---
+++ PART C (b).c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+{
+
+import socket
+
+def main():
+  """Start the client."""
+  client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  host = "localhost"
+  port = 8080
+  print("Enter a number: ")
+  number = input()
+  client_socket.sendto(number.encode(), (host, port))
+  data, address = client_socket.recvfrom(1024)
+  result = data.decode()
+  print("The result is: " + result)
+
+if __name__ == "__main__":
+  main()
+
+}
