7a6362316a2abc12a1dc23deeadcad598cb9b702
HIDAYAH298 <139749285+HIDAYAH298@users.noreply.github.com>
2023-07-17 16:19:26 +0800

    PART B (b).c

---
+++ PART B (b).c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+
+int main() {
+  int client_socket;
+  struct sockaddress_in server_address;
+  socklen_t address_size;
+  char buffer[1024];
+  int random_number;
+
+  // Create a socket.
+  client_socket = socket(AF_INET, SOCK_STREAM, 0);
+  if (client_socket < 0) {
+    perror("Error creating socket");
+    exit(1);
+  }
+
+  // Connect to the server.
+  server_address.sin_family = AF_INET;
+  server_address.sin_port = htons(8080);
+  server_address.sin_address.s_addr = inet_address("127.0.0.1");
+  addr_size = sizeof(server_address);
+  if (connect(client_socket, (struct sockaddress *) &server_address, address_size) < 0) {
+    perror("Error connecting to server");
+    exit(1);
+  }
+
+  // Receive the random number from the server.
+  recv(client_socket, buffer, 1024, 0);
+
+  // Print the random number.
+  random_number = atoi(buffer);
+  printf("Random number: %d\n", random_number);
+
+  // Close the socket.
+  close(client_socket);
+
+  return 0;
+}
